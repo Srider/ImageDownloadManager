e3a023858cf5be6f16142470fa34d820e61ba1e4
Srikar <srikar.ravela@tyfone.com>
2019-08-19 17:15:18 +0530

    Added ImageDownloadManager framework

---
+++ ImageDownloadManager.framework/Headers/ImageDownloadManager.h
@@ -0,0 +1,20 @@
+//
+//  ImageDownloadManager.h
+//  ImageDownloadManager
+//
+//  Created by Srikar on 19/08/19.
+//  Copyright © 2019 Srikar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageDownloadManager.
+FOUNDATION_EXPORT double ImageDownloadManagerVersionNumber;
+
+//! Project version string for ImageDownloadManager.
+FOUNDATION_EXPORT const unsigned char ImageDownloadManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageDownloadManager/PublicHeader.h>
+
